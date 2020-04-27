b472e3cf637d0a3cedfb0d30ac5104d3428732bb
Brayan Esneider Hernandez Martinez <bhernandez@asesoftware.com>
2020-04-27 16:20:19 -0500

    [ADD] added to mongoose useUnifiedTopology

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const container = require('./src/startup/container');
+const container = require("./src/startup/container");
 const server = container.resolve("app");
 const { MONGO_URI } = container.resolve("config");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-.then(() => server.start())
-.catch(console.log);
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => server.start())
+  .catch(console.log);
